2aed88695643e0c2bb8e74d82aaf075052f70099
Hafeezul Kareem <hafeezulkareem@gmail.com>
2023-12-23 17:26:45 +0530

    Update socket events

--- index.js
+++ index.js
@@ -16,42 +16,19 @@ app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-let activeUsersData = [];
-
 io.on("connection", (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
-
-  socket.on("disconnect", () => {
-    console.log(`🔥: ${socket.id} user disconnected`);
-    activeUsersData = activeUsersData.filter((user) => {
-      return user.socketId !== socket.id;
-    });
-  });
-
-  socket.on("addUser", (data) => {
-    // const peerIds = activeUsersData.map((d) => d.peerIds);
-    // io.to(data.socketId).emit("stream", peerIds);
-
-    const userDetails = {
-      name: data.name,
-      socketId: data.socketId,
-      // peerId: data.peerId,
-    };
-
-    activeUsersData.push(userDetails);
-  });
-
-  socket.on("chat message", (msg) => {
-    const target = activeUsersData.filter((d) => d.name === msg.name);
-    console.log("message: " + JSON.stringify(activeUsersData));
-    socket.to(target[0].socketId).emit("msgResponse", msg.msg);
-  });
-
   socket.on("addPeer", (data) => {
     const { roomId, peerId } = data;
 
     socket.join(roomId);
     socket.broadcast.emit("peerAdded", peerId);
+  });
+
+  socket.on("removePeer", (data) => {
+    const { roomId, peerId } = data;
+
+    socket.leave(roomId);
+    socket.broadcast.emit("peerRemoved", peerId);
   });
 });
 
