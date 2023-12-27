429fdcc5a6e60c6973b677138ececf0b83fdf194
Abhisaieswar <saieswarkunuthuru1@gmail.com>
2023-12-27 14:38:14 +0530

    added login and authentication apis

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors=require("cors")
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
+const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
 const server = createServer(app);
 const io = new Server(server, {
@@ -14,6 +19,34 @@ const io = new Server(server, {
 
 app.get("/", (req, res) => {
   res.send("hello world!");
+});
+
+app.post("/login", async (req,res)=>{
+  const {userName,password}=req.body;
+  const hashedPwd=await bcrypt.hash(password,10);
+  //store hashed pwd in db and verify pwd
+  const jwtToken= await jwt.sign({userName},"secret_key")
+  res.json({jwtToken})
+})
+
+app.get("/authenticate", (req, res) => {
+  const jwtToken = req.headers["authorization"]?.split(" ")[1];
+  
+  if (jwtToken === undefined) {
+    res.status(401);
+    res.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "secret_key", async (error, payload) => {
+      if(error) {
+        res.status(401)
+        res.send("Invalid Access Token")
+      }
+      else {
+        res.status(200)
+        res.send("Success")
+      }
+    });
+  }
 });
 
 io.on("connection", (socket) => {
