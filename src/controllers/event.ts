29c71ce08c0a0c927e2ea780a1375f46093688d2
Abhisaieswar <saieswarkunuthuru1@gmail.com>
2024-01-18 15:48:24 +0530

    added create and read apis for events

--- src/controllers/event.ts
+++ src/controllers/event.ts
@@ -1,4 +1,5 @@
 import DbConnectionManager from "../db/DbConnectionManager";
+import { Event } from "../db/entities/Event";
 
 const dbManager = DbConnectionManager.getInstance();
 
@@ -11,6 +12,44 @@ export const createEvent = async (req: any, res: any) => {
     res.status(200).send("Event created successfully!");
   } catch (err) {
     console.log(err);
+    res.status(500).json({ success: false, message: err });
+  }
+};
+
+export const getEventByEventId = async (req: any, res: any) => {
+  try {
+    const entityManager = await dbManager.getManager();
+    const { eventId } = req.body;
+
+    const eventData = await entityManager.find(Event, {
+      where: {
+        id: eventId,
+      },
+      relations: {
+        users: true,
+      },
+    });
+    res.status(200).send(eventData);
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+};
+
+export const getEventByUserId = async (req: any, res: any) => {
+  try {
+    const entityManager = await dbManager.getManager();
+    const { userId, startDate, endDate } = req.body;
+
+    const eventData = await entityManager
+      .createQueryBuilder(Event, "event")
+      .innerJoin("event.users", "user")
+      .where("user.id = :userId", { userId })
+      .andWhere("event.startDate >= :startDate", { startDate })
+      .andWhere("event.endDate <= :endDate", { endDate })
+      .getMany();
+
+    res.status(200).send(eventData);
+  } catch (err) {
     res.status(500).json({ success: false, message: err });
   }
 };
--- src/db/entities/Event.ts
+++ src/db/entities/Event.ts
@@ -3,7 +3,9 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
+  ManyToMany,
 } from "typeorm";
+import { User } from "./User";
 
 @Entity()
 export class Event {
@@ -42,8 +44,32 @@ export class Event {
 
   @CreateDateColumn()
   createdAt: Date;
+
+  @ManyToMany((type) => User, (user) => user.events, { cascade: true })
+  users: User[];
 }
 
 // SELECT * FROM events
 // WHERE user_id = 'user123'
 // AND (start_date <= '2024-01-13' AND (end_date >= '2024-01-13' OR end_date IS NULL));
+
+// CREATE TABLE IF NOT EXISTS "user_events_event" (
+//   "userId" INTEGER REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+//   "eventId" INTEGER REFERENCES "event" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+//   PRIMARY KEY ("userId", "eventId")
+// );
+
+// CREATE TABLE IF NOT EXISTS "event" (
+//   "id" SERIAL PRIMARY KEY,
+//   "title" VARCHAR(255) NOT NULL,
+//   "meetId" VARCHAR(255) NOT NULL,
+//   "startTime" TIMESTAMP NOT NULL,
+//   "endTime" TIMESTAMP NOT NULL,
+//   "startDate" TIMESTAMP NOT NULL,
+//   "endDate" TIMESTAMP,
+//   "isRecurring" BOOLEAN DEFAULT false,
+//   "recurringType" VARCHAR(255) NOT NULL,
+//   "expiresOn" TIMESTAMP,
+//   "response" VARCHAR(255) NOT NULL,
+//   "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// );
